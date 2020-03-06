f52b62937ac6f995840b15d873b7b2555f323dec
DuanJiaNing <aimeimeits@gmail.com>
2020-03-06 18:06:37 +0800

    add db schema

--- handler/mail/send.go
+++ handler/mail/send.go
@@ -1,11 +1,11 @@
 package mail
 
 import (
+	"gopkg.in/gomail.v2"
 	"net/http"
 
 	"GoMailer/app"
 	"GoMailer/handler"
-	"GoMailer/log"
 )
 
 func init() {
@@ -14,6 +14,28 @@ func init() {
 }
 
 func send(http.ResponseWriter, *http.Request) (interface{}, *app.ServerError) {
-	log.Info("got...")
+	dialer := gomail.NewDialer("smtp.qq.com", 465, "2213994603@qq.com", "athupcbmeyvvdjif")
+	err := dialer.DialAndSend(getMsg())
+	if err != nil {
+		return nil, app.Errorf(err, "failed to send email")
+	}
+
 	return nil, nil
+}
+
+func getMsg() *gomail.Message {
+	msg := gomail.NewMessage()
+	msg.SetHeader("From", msg.FormatAddress("2213994603@qq.com", "djnqq"))
+	msg.SetHeader("To", "djn163<duan_jia_ning@163.com>")
+	msg.SetHeader("Subject", "This is test mail")
+	html := `
+<div>
+<hr>
+<h1>H1 text</h1>
+<h2>H2 text</h2>
+<hr>
+</div>
+`
+	msg.SetBody("text/html", html)
+	return msg
 }
--- service/db/entity.go
+++ service/db/entity.go
@@ -4,30 +4,66 @@ import "time"
 
 type User struct {
 	ID         int64
-	Username   string
-	password   string
 	InsertTime time.Time
+
+	Username string
+	password string
 }
 
 type UserApp struct {
 	ID         int64
-	UserID     int64
-	AppName    string
-	Host       string
 	InsertTime time.Time
+
+	UserID int64
+
+	AppName string
+	Host    string
 }
 
-type AppApi struct {
+type EndPoint struct {
 	ID         int64
-	UserID     int64
-	UserAppID  int64
-	ApiID      int64
 	InsertTime time.Time
+
+	UserAppID       int64
+	DialerID        int64
+	TemplateID      int64
+	UserID          int64
+	DeliverStrategy int32
+	EnableReCaptcha bool
+
+	Name string
 }
 
-type Api struct {
+type Receiver struct {
 	ID         int64
-	URL        string
-	State      int32
 	InsertTime time.Time
+
+	EndPointID int64
+
+	Address      string
+	ReceiverType int32
+}
+
+type Dialer struct {
+	ID         int64
+	InsertTime time.Time
+
+	UserID int64
+
+	Host         string
+	Port         int32
+	AuthUsername string
+	AuthPassword string
+
+	Name string
+}
+
+type Template struct {
+	ID         int64
+	InsertTime time.Time
+
+	UserID int64
+
+	Template    string
+	ContentType string
 }
