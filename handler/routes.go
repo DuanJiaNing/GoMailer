bafa3049182d9c7c67b24941e497cef1170a63d1
DuanJiaNing <aimeimeits@gmail.com>
2020-03-26 12:02:35 +0800

    new router

--- handler/routes.go
+++ handler/routes.go
@@ -12,7 +12,26 @@ var (
 	APIRouter = Router.PathPrefix("/api/").Subrouter()
 
 	ShortcutRouter = APIRouter.Path("/shortcut").Subrouter()
+
+	// Root properties
+	// /api/*
 	MailRouter = APIRouter.PathPrefix("/mail/").Subrouter()
+	UserRouter = APIRouter.PathPrefix("/user/").Subrouter()
+
+	// User properties.
+	// /api/user/{UID}/*
+	DialerRouter   = UserRouter.PathPrefix("/{UID}/dialer").Subrouter()
+	TemplateRouter = UserRouter.PathPrefix("/{UID}/template").Subrouter()
+	AppRouter      = UserRouter.PathPrefix("/{UID}/app").Subrouter()
+
+	// User app properties.
+	// /api/user/{UID}/app/{AID}/*
+	EndPointRouter = AppRouter.PathPrefix("/{AID}/endpoint").Subrouter()
+
+	// End point properties.
+	// /api/user/{UID}/app/{AID}/endpoint/{EID}/*
+	PreferenceRouter = EndPointRouter.PathPrefix("/{EID}/preference").Subrouter()
+	ReceiverRouter   = EndPointRouter.PathPrefix("/{EID}/receiver").Subrouter()
 )
 
 func init() {
