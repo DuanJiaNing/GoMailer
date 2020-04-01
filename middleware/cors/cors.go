e1688af771fa335c0d69f6af1855a790e6796ab8
DuanJiaNing <aimeimeits@gmail.com>
2020-04-01 16:55:32 +0800

    handle allowOrigin

--- middleware/cors/cors.go
+++ middleware/cors/cors.go
@@ -11,23 +11,38 @@ import (
 	"GoMailer/log"
 )
 
+var (
+	freeAPI = map[string]struct{}{
+		"/api/shortcut": {},
+	}
+)
+
 func CORS(r *mux.Router) func(http.Handler) http.Handler {
 	// required so we don't get a code 405
 	r.Methods(http.MethodOptions).Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// No need to verify, already pass the Guard.
-			ak := key.EPKeyFromRequest(r)
-			ep, _ := endpoint.FindByKey(ak)
-			app, err := userapp.FindById(ep.UserAppId)
-			if err != nil {
-				log.Error("got error when find host for CORS origin: user app not exist")
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
+			allowOrigin := "*"
+			if _, ok := freeAPI[r.URL.Path]; !ok {
+				// No need to verify, already pass the Guard.
+				ak := key.EPKeyFromRequest(r)
+				ep, err := endpoint.FindByKey(ak)
+				if err != nil {
+					log.Error("got error when find host for CORS origin: end point not exist")
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				app, err := userapp.FindById(ep.UserAppId)
+				if err != nil {
+					log.Error("got error when find host for CORS origin: user app not exist")
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				allowOrigin = app.Host
 			}
 
-			w.Header().Set("Access-Control-Allow-Origin", app.Host)
+			w.Header().Set("Access-Control-Allow-Origin", allowOrigin)
 			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept-Encoding, User-Agent, Accept")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
